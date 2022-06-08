5286d4055086412cd05fa0980069bf14857251b5
Ha Xuan Thien <haxuanthien1@gmail.com>
2022-06-08 08:57:56 +0700

    Add files via upload

---
+++ Problemsets/CSES/Sorting and Searching/sum_of_four_values.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+using ll = long long;
+using str = string;
+using db = long double;
+ 
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using pdb = pair<db, db>;
+ 
+using vi = vector<int>;
+using vl = vector<ll>;
+using vdb = vector<db>;
+using vb = vector<bool>;
+using vs = vector<str>;
+using vpii = vector<pii>;
+using vpll = vector<pll>;
+using vpdb = vector<pdb>;
+#define IOS ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+ 
+//pairs
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)(x.size())
+#define mp make_pair
+#define fi first
+#define se second
+ 
+//vectors
+#define pb push_back
+#define pf push_front
+#define low_b lower_bound
+#define up_b upper_bound
+ 
+const int MOD = 1e9+7;
+//const int MOD = 998244353;
+const ll INF = 1e18;
+ 
+void solve(){
+    ll n, k;
+    cin >> n >> k;
+    vpll v(n);
+    for(int i = 0; i < n; i++){
+        cin >> v[i].fi;
+        v[i].se = i+1;
+    }
+    sort(all(v));
+    for(int i = 0; i < n; i++){
+        for(int j = i+1; j < n; j++){
+            ll val = k-v[i].fi-v[j].fi, l = j+1, r = n-1;
+            if(val <= 0)
+                break;
+            while(l < r){
+                if(v[l].fi+v[r].fi > val)
+                    r--;
+                else if(v[l].fi+v[r].fi < val)
+                    l++;
+                else{
+                    cout << v[i].se << " " << v[j].se << " " << v[l].se << " " << v[r].se << '\n';
+                    return;
+                }
+            }
+        }
+    }
+    cout << "IMPOSSIBLE" << '\n';
+}
+ 
+int main(){
+    IOS;
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    solve();
+}
