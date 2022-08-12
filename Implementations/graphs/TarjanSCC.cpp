088e1a86dc25054a472887425cd64229321a2d2a
Ha Xuan Thien <haxuanthien1@gmail.com>
2022-08-12 18:32:02 +0700

    Update TarjanSCC.cpp

--- Implementations/graphs/TarjanSCC.cpp
+++ Implementations/graphs/TarjanSCC.cpp
@@ -2,27 +2,29 @@
 
 const int MAXN = 2e5+77;
 int n, id = 1, scc = 0;
-bool onstack[MAXN];
 int num[MAXN], low[MAXN];
 vector<int> adj[MAXN];
 stack<int> stk;
 
 void dfs(int u){
-    onstack[u] = true;
     num[u] = low[u] = id++;
     stk.push(u);
-    for(int i = 0; i < adj[u].size(); i++){
-        if(!num[adj[u][i]])
-            dfs(adj[u][i]);
-        if(onstack[adj[u][i]])
-            low[u] = min(low[u], low[adj[u][i]]);
+    for(int v : adj[u]){
+        if(num[v] == -1)
+            continue;
+        if(!num[v]){
+            dfs(v);
+            low[u] = min(low[u], low[v]);
+        }
+        else
+            low[u] = min(low[u], num[v]);
     }
     if(low[u] == num[u]){
         while(stk.size()){
             int cur = stk.top();
             stk.pop();
-            onstack[cur] = false;
             low[cur] = low[u];
+            num[cur] = -1;
             if(cur == u)
                 break;
         }
