40b51e7e2e172f0f78cf41d8729a110dc825eece
Ha Xuan Thien <haxuanthien1@gmail.com>
2022-10-06 00:14:57 +0700

    Add files via upload

---
+++ Implementations/data-structures/BinaryTrie.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int LIM = 30;
+
+struct Node{
+    Node* child[2];
+    int cnt;
+    Node(){
+        for(int i = 0; i < 2; i++)
+            child[i] = NULL;
+        cnt = 0;
+    }
+};
+Node* root = new Node();
+
+void trieInsert(int x){
+    Node* p = root;
+    for(int i = LIM; i >= 0; i--){
+        int bit = ((x >> i) & 1);
+        if(p->child[bit] == NULL)
+            p->child[bit] = new Node();
+        p = p->child[bit];
+        p->cnt++;
+    }
+}
+
+void trieErase(int x){
+    Node* p = root;
+    for(int i = LIM; i >= 0; i--){
+        int bit = ((x >> i) & 1);
+        p = p->child[bit];
+        p->cnt--;
+    }
+}
