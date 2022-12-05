8ca1888245ca35a7cf615bf82b3f34f52120c573
Ha Xuan Thien <haxuanthien1@gmail.com>
2022-12-05 10:05:43 +0700

    Add files via upload

--- Implementations/geometry/Point.cpp
+++ Implementations/geometry/Point.cpp
@@ -4,7 +4,9 @@ struct Point{
     Point(T _x = 0, T _y = 0) : x(_x), y(_y) {}
 
     bool operator < (Point p) const { return tie(x, y) < tie(p.x, p.y); }
+    bool operator > (Point p) const { return tie(x, y) > tie(p.x, p.y); }
     bool operator == (Point p) const { return tie(x, y) == tie(p.x, p.y); }
+    bool operator != (Point p) const { return tie(x, y) != tie(p.x, p.y); }
 
     Point operator + (Point p) const { return Point(x + p.x, y + p.y); }
     Point operator - (Point p) const { return Point(x - p.x, y - p.y); }
@@ -19,3 +21,9 @@ struct Point{
     friend ostream& operator << (ostream &os, const Point &p) {
         return os << "(" << p.x << ", " << p.y << ")"; }
 };
+
+ll sgn(const ll &x) { return (x >= 0 ? (x ? 1 : 0) : -1); }
+
+ll ccw(const Point &P0, const Point &P1, const Point &P2){
+    return (P1 - P0) ^ (P2 - P1);
+}
