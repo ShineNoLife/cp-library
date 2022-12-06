589a5884072735e6ebbf25a5b20d2ba793f6ca21
Ha Xuan Thien <haxuanthien1@gmail.com>
2022-12-06 13:04:34 +0700

    Update InsideConvex.cpp

--- Implementations/geometry/InsideConvex.cpp
+++ Implementations/geometry/InsideConvex.cpp
@@ -2,33 +2,18 @@ ll ccw(const Point &P0, const Point &P1, const Point &P2){
     return (P1 - P0) ^ (P2 - P1);
 }
 
-ll sgn(const ll &x) { return (x >= 0 ? (x ? 1 : 0) : -1); }
-
 bool insideConvex(Point p, vector<Point> &poly){
     int n = sz(poly);
-    ll cl, cr;
-    
+    if(ccw(poly[0], poly[1], p) >= 0) return false;
+    if(ccw(poly[n - 1], poly[0], p) >= 0) return false;
+
     ll l = 1, r = n-1;
     while(l < r){
         ll mid = (l+r+1)/2;
         if(ccw(poly[0], p, poly[mid]) >= 0) l = mid;
         else r = mid - 1;
     }
-    cl = l;
+    r = l + 1;
 
-    l = 1, r = n-1;
-    while(l < r){
-        ll mid = (l+r)/2;
-        if(ccw(poly[0], p, poly[mid]) <= 0) r = mid;
-        else l = mid + 1;
-    }
-    cr = r;
-
-    if(cl == cr) return ((cl == 1 || cl == n-1) ? false : true);
-    ll e1, e2, e3;
-    e1 = sgn(ccw(poly[0], poly[cl], p));
-    e2 = sgn(ccw(poly[cl], poly[cr], p));
-    e3 = sgn(ccw(poly[cr], poly[0], p));
-
-    return (e1 == e2 && e2 == e3);
+    return (ccw(poly[l], p, poly[r]) > 0);
 }
