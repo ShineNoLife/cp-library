a75aa7db75c15c8988f28c2b4d91f9f4da09dd9f
Ha Xuan Thien <haxuanthien1@gmail.com>
2022-12-11 20:32:31 +0700

    Add files via upload

---
+++ Implementations/data-structures/ConvexHullTrick.cpp
@@ -0,0 +1,29 @@
+struct Line {
+    mutable ll m, b, p;
+    bool operator < (const Line& o) const { return m < o.m; }
+    bool operator < (ll x) const { return p < x; }
+};
+
+struct CHT : multiset<Line, less<>> {
+    static const ll inf = LLONG_MAX;
+    ll div(ll a, ll b) {
+        return a / b - ((a ^ b) < 0 && a % b); }
+    bool isect(iterator x, iterator y) {
+        if (y == end()) return x->p = inf, 0;
+        if (x->m == y->m) x->p = x->b > y->b ? inf : -inf;
+        else x->p = div(y->b - x->b, x->m - y->m);
+        return x->p >= y->p;
+    }
+    void addLine(ll m, ll b) {
+        auto z = insert({m, b, 0}), y = z++, x = y;
+        while (isect(y, z)) z = erase(z);
+        if (x != begin() && isect(--x, y)) isect(x, y = erase(y));
+        while ((y = x) != begin() && (--x)->p >= y->p)
+            isect(x, erase(y));
+    }
+    ll query(ll x) {
+        assert(!empty());
+        auto l = *lower_bound(x);
+        return l.m * x + l.b;
+    }
+};
