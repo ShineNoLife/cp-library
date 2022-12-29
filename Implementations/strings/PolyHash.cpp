5ace4f0209110ad2f197e4b977dd62906e637d25
Ha Xuan Thien <haxuanthien1@gmail.com>
2022-12-29 16:21:07 +0700

    Update PolyHash.cpp

--- Implementations/strings/PolyHash.cpp
+++ Implementations/strings/PolyHash.cpp
@@ -1,6 +1,6 @@
 const int maxn = 2e5 + 250;
-const int nMOD = 3;
-const int modulo[] = {(int)1e9 + 7, (int)1e9 + 4777, (int)1e9 + 10777};
+const int nMOD = 2;
+const int modulo[] = {(int)1e9 + 7, (int)1e9 + 3777, (int)1e9 + 10777, (int)1e9 + 19777};
 // 'z' is 122 is ascii
 const int BASE = 333;
 int pw[nMOD][maxn + 1];
@@ -14,18 +14,32 @@ void precalc(){
 }
   
 struct PolyHash{
-    int val[nMOD];
+    int val[nMOD], len;
   
-    PolyHash() { mem(val, 0); }
+    PolyHash() { mem(val, 0), len = 0; }
  
-    PolyHash(const string &s) {
-        mem(val, 0);
+    PolyHash(const string &s){
+        mem(val, 0), len = sz(s);
  
         for(int j = 0; j < nMOD; j++)
             for(int i = 0; i < sz(s); i++)
                 val[j] = (1ll * val[j] * BASE + s[i]) % modulo[j];
     }
  
+    void addFront(char c){
+        for(int j = 0; j < nMOD; j++)
+            val[j] = (1ll * c * pw[j][len] + val[j]) % modulo[j];
+
+        len++;
+    }
+
+    void addBack(char c){
+        len++;
+
+        for(int j = 0; j < nMOD; j++)
+            val[j] = (1ll * val[j] * BASE + c) % modulo[j];
+    }
+
     bool operator < (const PolyHash &x) const {
         for(int i = 0; i < nMOD; i++) 
             if(val[i] != x.val[i]) 
