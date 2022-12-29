ac659adffcb3e2feb6e5d023b5dc2950cabb19b7
Ha Xuan Thien <haxuanthien1@gmail.com>
2022-12-29 17:02:55 +0700

    Update PolyHash.cpp

--- Implementations/strings/PolyHash.cpp
+++ Implementations/strings/PolyHash.cpp
@@ -1,6 +1,6 @@
 const int maxn = 2e5 + 250;
 const int nMOD = 2;
-const int modulo[] = {(int)1e9 + 7, (int)1e9 + 3777, (int)1e9 + 10777, (int)1e9 + 19777};
+const int mods[] = {(int)1e9 + 7, (int)1e9 + 3777, (int)1e9 + 10777, (int)1e9 + 19777};
 // 'z' is 122 is ascii
 const int BASE = 333;
 int pw[nMOD][maxn + 1];
@@ -9,35 +9,29 @@ void precalc(){
     for(int j = 0; j < nMOD; j++){
         pw[j][0] = 1;
         for(int i = 1; i <= maxn; i++)
-            pw[j][i] = (1ll * pw[j][i - 1] * BASE) % modulo[j];
+            pw[j][i] = (1ll * pw[j][i - 1] * BASE) % mods[j];
     }
 }
   
 struct PolyHash{
-    int val[nMOD], len;
+    vector<int> val[nMOD];
+    int n;
   
-    PolyHash() { mem(val, 0), len = 0; }
+    PolyHash() { n = 0; }
  
     PolyHash(const string &s){
-        mem(val, 0), len = sz(s);
+        n = sz(s);
  
-        for(int j = 0; j < nMOD; j++)
-            for(int i = 0; i < sz(s); i++)
-                val[j] = (1ll * val[j] * BASE + s[i]) % modulo[j];
+        for(int j = 0; j < nMOD; j++){
+            val[j].pb(s[0]);
+            for(int i = 0; i < n; i++)
+                val[j].pb((1ll * val[j][i - 1] * BASE + s[i]) % mods[j]);
+        }
     }
  
-    void addFront(char c){
-        for(int j = 0; j < nMOD; j++)
-            val[j] = (1ll * c * pw[j][len] + val[j]) % modulo[j];
-
-        len++;
-    }
-
-    void addBack(char c){
-        len++;
-
-        for(int j = 0; j < nMOD; j++)
-            val[j] = (1ll * val[j] * BASE + c) % modulo[j];
+    int Query(int j, int l, int r){
+        return (1ll * val[j][r] - (l - 1 >= 0 ? 1ll * val[j][l - 1] * pw[j][r - l + 1] : 0)
+             + 1ll * mods[j] * mods[j]) % mods[j];
     }
 
     bool operator < (const PolyHash &x) const {
