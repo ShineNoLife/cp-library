4d20935390c2430f2fbe626bd60ad4f5a04f1b3b
Ha Xuan Thien <haxuanthien1@gmail.com>
2022-12-29 15:10:44 +0700

    Add files via upload

--- Implementations/strings/PolyHash.cpp
+++ Implementations/strings/PolyHash.cpp
@@ -1,36 +1,36 @@
 const int maxn = 2e5 + 250;
-const int nMOD = 2;
-const int modulo[] = {(int)1e9 + 2277, (int)1e9 + 5277};
+const int nMOD = 3;
+const int modulo[] = {(int)1e9 + 7, (int)1e9 + 4777, (int)1e9 + 10777};
 // 'z' is 122 is ascii
-const int BASE = 303;
-int pw[nMOD][maxn];
-
+const int BASE = 333;
+int pw[nMOD][maxn + 1];
+ 
 void precalc(){
     for(int j = 0; j < nMOD; j++){
         pw[j][0] = 1;
-        for(int i = 1; i < maxn; i++)
-            pw[j][i] = (1LL * pw[j][i - 1] * BASE) % modulo[j];
+        for(int i = 1; i <= maxn; i++)
+            pw[j][i] = (1ll * pw[j][i - 1] * BASE) % modulo[j];
     }
 }
- 
+  
 struct Hash{
     int val[nMOD];
- 
+  
     Hash() { mem(val, 0); }
-
+ 
     Hash(const string &s) {
         mem(val, 0);
-        
+ 
         for(int j = 0; j < nMOD; j++)
             for(int i = 0; i < sz(s); i++)
-                val[j] = (val[j] + 1ll * s[i] * pw[j][i]) % modulo[j];
+                val[j] = (1ll * val[j] * BASE + s[i]) % modulo[j];
     }
-
+ 
     bool operator < (const Hash &x) const {
         for(int i = 0; i < nMOD; i++) 
             if(val[i] != x.val[i]) 
                 return (val[i] < x.val[i]);
-
+ 
         return false;
     }
 };
