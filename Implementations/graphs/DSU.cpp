df50c8c3675d5d9798c7eeee395b5a27b5e7cd9a
Ha Xuan Thien <haxuanthien1@gmail.com>
2023-01-03 16:30:16 +0700

    Add files via upload

--- Implementations/graphs/DSU.cpp
+++ Implementations/graphs/DSU.cpp
@@ -1,37 +1,27 @@
-/*
-Description: Disjoin set union implementation
-Optimizations: path compression and balanced trees with log(n) height
-*/
-#include <bits/stdc++.h>
-using namespace std;
+struct DSU{
+    vector<int> par, rnk;
 
-const int MAXN = 2e5+77;
-int par[MAXN], rnk[MAXN];
+    DSU(int n) { 
+        rnk.assign(n + 1, 0);
+        par.assign(n + 1, 0);
 
-void setup(int n){
-    for(int i = 0; i < n; i++){
-        par[i] = i;
-        rnk[i] = 1;
+        for(int i = 0; i <= n; i++)
+            par[i] = i, rnk[i] = 1;
     }
-}
 
-int root(int u){
-    if(par[u] == u)
-        return par[u];
-    par[u] = root(par[u]);
-    return par[u];
-}
+    int root(int u){
+        return (par[u] == u ? u : par[u] = root(par[u]));
+    }
 
-void unite(int x, int y){
-    int p1 = root(x), p2 = root(y);
-    if(p1 == p2)
-        return;
-    if(rnk[p1] > rnk[p2])
-        par[p2] = p1;
-    else
-        par[p1] = p2, rnk[p2] = max(rnk[p2], rnk[p1]+1);
-}
+    bool unite(int x, int y){
+        int u = root(x), v = root(y);
 
-int main(){
+        if(u == v) return false;
+        if(rnk[u] < rnk[v]) swap(u, v);
 
-}
+        par[v] = u;
+        rnk[u] += rnk[v];
+
+        return true;
+    }
+};
