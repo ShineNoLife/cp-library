6ca74c635d6d051971e9fb59e26826635106fbb1
Ha Xuan Thien <haxuanthien1@gmail.com>
2023-01-14 23:43:30 +0700

    Update 1DBIT.cpp

--- Implementations/data-structures/1DBIT.cpp
+++ Implementations/data-structures/1DBIT.cpp
@@ -1,51 +1,35 @@
 /*
 Description: Binary indexed tree in one dimension
 Time complexity: Query and single point update in O(logn)
-                 Initialization in O(n)
 */
+
 #include <bits/stdc++.h>
 using namespace std;
 
 class BIT1D{
-    typedef int T;
+public:
     int n;
-    vector<T> BIT;
-    T query(int pos){
-        long long ans = 1;
-        while(pos > 0){
-            ans+=BIT[pos];
-            pos-=(pos&(-pos));
-        }
-        return ans;
+    vector<ll> BIT;
+
+    BIT1D(int _n){
+        n = _n;
+        BIT.assign(n + 1, 0);
     }
-    void updatef(int pos, long long val){  
-        while(pos <= n){
-            BIT[pos]+=val;
-            pos+=(pos&(-pos));
-        }
+
+    void reset(){
+        BIT.assign(n + 1, 0);
     }
-    public:
-        void setup(const vector<T> &a){
-            n = (int)a.size();
-            BIT.resize(n+1, 0);
-            for(int i = 1; i <= n; i++){
-                BIT[i]+=a[i-1];
-                if(i+(i&(-i)) <= n)
-                    BIT[i+(i&(-i))]+=BIT[i];
-            }
-        }
-        T query(int l, int r){
-            l++; r++;
-            if(l > r)
-                return 0LL;
-            return query(r)-query(l-1);
-        }
-        void update(int pos, long long val){
-            pos++;
-            updatef(pos, val);
-        }
+
+    ll Query(int p){
+        ll res = 0;
+        for(; p > 0; p -= (p & (-p)))
+            res = max(res, BIT[p]);
+
+        return res;
+    }
+
+    void Update(int p, ll x){  
+        for(; p <= n; p += (p & (-p)))
+            BIT[p] = max(BIT[p], x);
+    }
 };
-
-int main(){
-
-}
