6264f9dd18ede590f00559dbaf2a8af688cee214
Ha Xuan Thien <haxuanthien1@gmail.com>
2023-04-08 14:15:38 +0700

    Update BinomialCoefficient.cpp

--- Implementations/math/BinomialCoefficient.cpp
+++ Implementations/math/BinomialCoefficient.cpp
@@ -1,27 +1,30 @@
 const int MOD = 1e9+7;
-const int maxn = 1e6;
-ll fact[maxn+1];
+const int maxn = 1e6 + 16;
+ll fact[maxn + 1];
 
-ll binpow(ll a, ll b){
-    ll ans = 1;
-    while(b){
-        if(b&1)
-            ans = (ans*a)%MOD;
-        a = (a*a)%MOD;
+ll binpow(ll a, ll b) {
+    ll res = 1;
+
+    while(b) {
+        if (b & 1)
+            res = (res * a) % MOD;
+
+        a = (a * a) % MOD;
         b >>= 1;
     }
-    return ans;
+
+    return res;
 }
 
-void FactSetup(){
+void FactSetup() {
     fact[0] = 1;
-    for(int i = 1; i <= maxn; i++)
-        fact[i] = (fact[i-1]*i)%MOD;
+    for (int i = 1; i <= maxn; i++)
+        fact[i] = (fact[i-1] * i) % MOD;
 }
 
-ll C(int n, int k){
+ll C(int n, int k) {
     if(k > n) 
         return 0;
-    return ((fact[n]*binpow(fact[k], MOD-2))%MOD*binpow(fact[n-k], MOD-2))%MOD;
+    return ((fact[n] * binpow(fact[k], MOD - 2)) % MOD * binpow(fact[n - k], MOD - 2)) % MOD;
 }
 
