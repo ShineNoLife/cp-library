e4d8a640bcb61f7f9f87f1db22acf127c3010414
Ha Xuan Thien <haxuanthien1@gmail.com>
2023-05-12 21:45:06 +0700

    Update DSU.cpp

--- Implementations/graphs/DSU.cpp
+++ Implementations/graphs/DSU.cpp
@@ -1,27 +1,29 @@
-struct DSU{
+struct DSU {
+    int n;
     vector<int> par, rnk;
-
-    DSU(int n) { 
-        rnk.assign(n + 1, 0);
+ 
+    DSU(int _n) {
+        n = _n;
         par.assign(n + 1, 0);
-
-        for(int i = 0; i <= n; i++)
+        rnk.assign(n + 1, 0);
+ 
+        for (int i = 0; i <= n; i++)
             par[i] = i, rnk[i] = 1;
     }
-
-    int root(int u){
+ 
+    int root(int u) {
         return (par[u] == u ? u : par[u] = root(par[u]));
     }
-
-    bool unite(int x, int y){
+ 
+    bool unite(int x, int y) {
         int u = root(x), v = root(y);
-
-        if(u == v) return false;
-        if(rnk[u] < rnk[v]) swap(u, v);
-
+ 
+        if (u == v) return false;
+        if (rnk[u] < rnk[v]) swap(u, v);
+ 
         par[v] = u;
         rnk[u] += rnk[v];
-
+ 
         return true;
     }
 };
