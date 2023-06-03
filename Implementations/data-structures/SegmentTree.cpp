0194a5b4dfb7ce0070981166258144d69a07f889
Ha Xuan Thien <haxuanthien1@gmail.com>
2023-06-04 00:19:33 +0700

    Update SegmentTree.cpp

--- Implementations/data-structures/SegmentTree.cpp
+++ Implementations/data-structures/SegmentTree.cpp
@@ -1,24 +1,26 @@
-class SegTree{
+class SegTree {
 public:
     SegTree *left = NULL, *right = NULL;
     ll lazy, val;
+    
 #define mid ((tl+tr) >> 1)
     SegTree(int tl, int tr) : lazy(0), val(0) {
-        if(tl == tr){
+        if (tl == tr) {
             val = 0;
             return;
         }
 
         left = new SegTree(tl, mid, v);
-        right = new SegTree(mid+1, tr, v);
+        right = new SegTree(mid + 1, tr, v);
+
         Pull();
     }
 
-    void Update(int tl, int tr, int l, int r, ll x){
+    void Update(int tl, int tr, int l, int r, ll x) {
         if(r < tl || tr < l)
             return;
 
-        if(l <= tl && tr <= r){
+        if (l <= tl && tr <= r) {
             val += x;
             lazy += x;
             return;
@@ -26,11 +28,11 @@ public:
 
         Push();
         left->Update(tl, mid, l, r, x);
-        right->Update(mid+1, tr, l, r, x);
+        right->Update(mid + 1, tr, l, r, x);
         Pull();
     }
 
-    ll Query(int tl, int tr, int l, int r){
+    ll Query(int tl, int tr, int l, int r) {
         if(r < tl || tr < l)
             return INF;
 
@@ -38,18 +40,21 @@ public:
             return val;
 
         Push();
-        return min(left->Query(tl, mid, l, r), right->Query(mid+1, tr, l, r));
+
+        return min(left->Query(tl, mid, l, r), right->Query(mid + 1, tr, l, r));
     }
 
-    void Push(){
+    void Push() {
         left->val += lazy;
         right->val += lazy;
+
         left->lazy += lazy;
         right->lazy += lazy;
+
         lazy = 0;
     }
 
-    void Pull(){
+    void Pull() {
         val = min(left->val, right->val);
     }
 #undef mid
