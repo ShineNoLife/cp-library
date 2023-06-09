98550616709409fb77f3b1f43b23bc3b9e20506a
Ha Xuan Thien <haxuanthien1@gmail.com>
2023-06-09 13:32:12 +0700

    Update CentroidDecomposition.cpp

--- Implementations/graphs/CentroidDecomposition.cpp
+++ Implementations/graphs/CentroidDecomposition.cpp
@@ -1,13 +1,13 @@
 const int maxn = 2e5 + 25;
 vector<ll> adj[maxn];
 ll sub[maxn], par[maxn];
-bool vis[maxn];
+bool del[maxn];
 
 int dfsSz(int u, int p) {
     sub[u] = 1;
 
     for (int v : adj[u]) {
-        if (vis[v] || v == p) continue;
+        if (del[v] || v == p) continue;
 
         sub[u] += dfsSz(v, u);
     }
@@ -17,7 +17,7 @@ int dfsSz(int u, int p) {
 
 int findCentroid(int u, int p, int dsz) {
     for (int v : adj[u]) {
-        if (vis[v] || v == p) continue;
+        if (del[v] || v == p) continue;
 
         if (sub[v] > (dsz >> 1)) return findCentroid(v, u, dsz);
     }
@@ -29,10 +29,10 @@ void centroidDecompose(int u, int p = -1) {
     int cen = findCentroid(u, p, dfsSz(u, p));
 
     par[cen] = (p == -1 ? cen : p);
-    vis[cen] = true;
+    del[cen] = true;
 
     for (int v : adj[cen]) {
-        if (vis[v]) continue;
+        if (del[v]) continue;
 
         centroidDecompose(v, cen);
     }
