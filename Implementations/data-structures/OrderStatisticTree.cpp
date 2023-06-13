29aa161cbc3afe1e12b7f283d29fec725d12b6b8
Ha Xuan Thien <haxuanthien1@gmail.com>
2023-06-13 08:30:24 +0700

    Add files via upload

---
+++ Implementations/data-structures/OrderStatisticTree.cpp
@@ -0,0 +1,8 @@
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+
+template<class T>
+using OrderedTree = tree<T, null_type, less<T>, rb_tree_tag,
+    tree_order_statistics_node_update>;
