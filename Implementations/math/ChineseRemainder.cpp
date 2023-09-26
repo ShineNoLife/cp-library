b84604f18fd25f5a5a24a245b091bcf2bf0dcf53
Ha Xuan Thien <haxuanthien1@gmail.com>
2023-09-26 16:22:09 +0700

    Update ChineseRemainder.cpp

--- Implementations/math/ChineseRemainder.cpp
+++ Implementations/math/ChineseRemainder.cpp
@@ -1,33 +1,41 @@
-struct euclid_T { ll x, y, d; };
-
-euclid_T ex_euclid(ll a, ll b){
+struct gcd_t { ll x, y, d; };
+ 
+gcd_t e_gcd(ll a, ll b) {
     if (b == 0) return {1, 0, a};
+ 
+    gcd_t res = e_gcd(b, a % b);
 
-    euclid_T res = ex_euclid(b, a % b);
-    return {res.y, res.x - a / b * res.y, res.d};
+    return {res.y, res.x - res.y * (a / b), res.d};
 }
 
-pll crt(vector<ll> r, vector<ll> m){
+pll crt(vector<ll> r, vector<ll> m) {
     //find x such that for (1 <= i <= n): x = r[i] (mod m[i]) 
     //return {y, z} where x = y (mod z), z = lcm of vector m
     //all solutions are congruent modulo z
-
+ 
     ll y = r[0], z = m[0];
-    for(int i = 1; i < sz(r); i++){
-        euclid_T cur = ex_euclid(z, m[i]);
-
+    for (int i = 1; i < sz(r); i++) {
+        gcd_t cur = e_gcd(z, m[i]);
+            
         ll x = cur.x, d = cur.d;
-
-        if((r[i] - y) % d != 0) return {0, 0};
-
-        //ca mod cb = c(a mod b)
-        //y = y + (x * (r[i] - y) / d * z) % (m[i] / d * z)
-        y = y + ((x * (r[i] - y) / d) % (m[i] / d)) * z;
+            
+        if((r[i] - y) % d != 0) return {-1, -1};
+        
+        //ka = kb (mod kc)   =>   a = b (mod c) if (gcd(k, c) = 1)
+        //add (x * (r[i] - y) / d * z) to result (with moduli lcm(z, m[i]))
+        ll tmp = (x * (r[i] - y) / d) % (m[i] / d);
+        y = y + tmp * z;
         z = z / d * m[i];
-
+        
         y %= z;
-        if(y < 0) y += z;
+        if (y < 0) y += z;
     }
-
+ 
     return {y, z};
+}
+
+ll inverse(ll a, ll m) {
+    gcd_t cur = e_gcd(a, m);
+ 
+    return (cur.x % m + m) % m;
 }
