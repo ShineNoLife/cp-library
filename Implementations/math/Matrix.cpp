ad9bfa2c16d529c077271ed8ec2ae292bf70ddad
Ha Xuan Thien <haxuanthien1@gmail.com>
2023-11-13 00:29:24 +0700

    Update Matrix.cpp

--- Implementations/math/Matrix.cpp
+++ Implementations/math/Matrix.cpp
@@ -1,20 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Matrix{
-    long long A[2][2];
-    auto & operator [](int i){
+struct Matrix {
+    int n, m, A[217][217];
+
+    auto & operator [](int i) {
         return A[i];
     }
-    Matrix(){
+
+    Matrix() {
         mem(A, 0);
     }
-    Matrix operator *(Matrix B){
-        Matrix C;
-        for(int i = 0; i < 2; i++)
-            for(int j = 0; j < 2; j++)
-                for(int k = 0; k < 2; k++)
-                    C[i][j]+=A[i][k]*B[k][j], C[i][j]%=MOD;
+
+    Matrix(int _n, int _m) {
+        n = _n, m = _m;
+
+        mem(A, 0);
+    }
+
+    Matrix operator *(Matrix B) {
+        Matrix C(n, B.m);
+
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < B.m; j++)
+                for(int k = 0; k < m; k++)
+                    C[i][j] = (1LL * C[i][j] + 1LL * A[i][k] * B[k][j]) % MOD;
+
         return C;
     }
 };
